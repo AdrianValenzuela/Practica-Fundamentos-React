d9e20d7c1e4ff71e7e59b5b765ee94a0b97c4761
Adrian Valenzuela <avgvalenzuela@gmail.com>
2021-04-10 16:27:06 +0200

    estilos pagina detalle

--- nodepop/src/components/adverts/AdvertDetailPage/AdvertDetailPage.js
+++ nodepop/src/components/adverts/AdvertDetailPage/AdvertDetailPage.js
@@ -5,6 +5,8 @@ import { Redirect } from 'react-router-dom';
 // local imports
 import advertsService from '../../../api/adverts.js';
 import { Layout } from '../../layout';
+import { Button } from '../../shared';
+import './AdvertDetailPage.css';
 
 function AdvertDetailPage({ ...props }) {
     const [advert, setAdvert] = React.useState(null)
@@ -32,17 +34,26 @@ function AdvertDetailPage({ ...props }) {
 }
 
 function AdvertDetails({ advert }) {
+
+    const defaultImage = 'https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png'
+
     return (
         <div className='details'>
             <h1 className='title'>{advert.name}</h1>
             <div className='advert-details'>
-                <img src={advert.photo} alt={advert.name} />
+                <img className='advert-image' src={advert.photo ? advert.photo : defaultImage} alt={advert.name} />
                 <div className='advert'>
                     <span>{`Price: ${advert.price}`}</span>
                     <span>{`Status: ${advert.sale ? 'Sale' : 'Purchase'}`}</span>
                     <span>{`Tags: ${advert.tags}`}</span>
                 </div>
             </div>
+            <div>
+                <Button 
+                    className={'button is-danger is-rounded'}
+                    text={'Delete'}
+                />
+            </div>            
         </div>
     );
 }
